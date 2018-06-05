08824cd3829bb0abaf84874f5256b8ce571c0974
bat17i12 <Baiysh.Toktosunov.i12@fh-zwickau.de>
2018-06-05 23:57:09 +0200

    Added Bottom Navigation

---
+++ app/src/main/java/de/fh_zwickau/timemanagement2/MainActivity.java
@@ -0,0 +1,46 @@
+package de.fh_zwickau.timemanagement2;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper.disableShiftMode(bottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_links:
+                            selectedFragment = new LinksFragment();
+                            break;
+                        case  R.id.nav_add:
+                            selectedFragment = new AddFragment();
+                            break;
+                        case  R.id.nav_quest:
+                            selectedFragment = new QuestFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+}
