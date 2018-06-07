1ad4c231ccc415acb76a3f8cb5e6a1ebbf848ff6
zht17i11 <Zhanysh.Toktosunov.i11@fh-zwickau.de>
2018-06-07 22:15:43 +0200

    Added new enum - Urgency

---
+++ app/src/main/java/de/fh_zwickau/timemanagement2/Urgency.java
@@ -0,0 +1,8 @@
+package de.fh_zwickau.timemanagement2;
+
+public enum Urgency {
+    DRWG, // Dringend & Wichtig
+    NDRWG, //Nicht dringlich & Wichtig
+    DRNWG, //Dringend & Nicht wichtig
+    NDRNWG //Nicht dringlich & Nicht wichtig
+}
