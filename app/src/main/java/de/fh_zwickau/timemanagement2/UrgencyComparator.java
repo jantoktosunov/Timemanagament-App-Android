4cba99b36c73630a909b847bd3957ab310095e19
zht17i11 <Zhanysh.Toktosunov.i11@fh-zwickau.de>
2018-06-14 00:37:55 +0200

    Added UrgencyComparator.java

---
+++ app/src/main/java/de/fh_zwickau/timemanagement2/UrgencyComparator.java
@@ -0,0 +1,15 @@
+package de.fh_zwickau.timemanagement2;
+
+import java.util.Comparator;
+
+public class UrgencyComparator implements Comparator<Task> {
+    @Override
+    public int compare(Task t1, Task t2) {
+        if(t1.getUrgency().getOrder()>t2.getUrgency().getOrder()){
+            return 1;
+        } else {
+            return -1;
+        }
+
+    }
+}
