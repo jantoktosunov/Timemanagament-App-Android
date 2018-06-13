f4c52687da20a8a679f1ebd000f8930438eb2dcd
zht17i11 <Zhanysh.Toktosunov.i11@fh-zwickau.de>
2018-06-14 00:38:49 +0200

    Changed Urgency - added field - int order

--- app/src/main/java/de/fh_zwickau/timemanagement2/Urgency.java
+++ app/src/main/java/de/fh_zwickau/timemanagement2/Urgency.java
@@ -4,8 +4,15 @@ package de.fh_zwickau.timemanagement2;
  *
  */
 public enum Urgency {
-    UI, // Urgent & Important
-    NUI, //Not Urgent & Important
-    UNI, //Urgent & Not important
-    NUNI //Not Urgent & Not Important
+    UI(1), // Urgent & Important
+    UNI(2), //Urgent & Not important
+    NUI(3), //Not Urgent & Important
+    NUNI(4); //Not Urgent & Not important
+    private final int order;
+    private Urgency(int order){
+        this.order = order;
+    }
+    public int getOrder(){
+        return order;
+    }
 }
